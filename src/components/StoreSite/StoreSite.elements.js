2db30c39bdecb48a6ab879048121e7554db7fdce
Caro <55109108+CaroMen@users.noreply.github.com>
2020-10-25 22:40:33 -0500

    Store

--- src/components/StoreSite/StoreSite.elements.js
+++ src/components/StoreSite/StoreSite.elements.js
@@ -1,40 +1,89 @@
 import styled from 'styled-components';
+import { AiOutlineStar } from "react-icons/ai";
 
 export const StoreContainer = styled.div`
 display: grid;
 grid-template-columns: 1fr 1fr 1fr 1fr;
+margin-top: 100px;
+margin-bottom: 100px;
+padding-left: 150px;
+padding-right: 150px;
 `
 
 export const StoreGrids = styled.div`
 display: grid;
-grid-template-rows: 1fr 1fr;
-background-color: #BAB5BA;
-border-radius: 15px;
-margin-top: 100px;
-max-width: 80%;
-max-height: 70%;
-padding: 50px 50px 50px 50px;
+grid-template-rows: 1fr .5fr;
+/* background-color: #F7F4F3; */
+/* border-radius: 15px; */
+/* margin-right: -18px; */
+/* grid-column-gap: -150px; */
+max-width:100%;
+max-height:100%;
+/* object-fit: contain; */
+border-top: 3px solid #bbb;
+padding: 20px 20px 20px 20px;
+/* padding-left: 500px; */
 margin-left: 30px;
 margin-right: 30px;
-box-shadow: -5px -4px 3px 0px rgba(255, 255, 255, 0.15),
-            5px 4px 3px 0px rgba(88, 88, 88, 0.15);
+/* box-shadow: -5px -4px 3px 0px rgba(255, 255, 255, 0.15),
+            5px 4px 3px 0px rgba(88, 88, 88, 0.15); */
 
 `
 
 export const ImgStore = styled.img`
 width: 100%;
 height: 100%;
-margin-bottom: -130px;
+/* margin-bottom: -130px; */
+object-fit: fill;
+display: block;
 `
 
 export const ItemHeadline = styled.div`
 /* margin-bottom: 24px; */
-padding-top: 30px;
-font-size: 44px;
+margin-top: -170px;
+font-size: 34px;
 line-height: 1.2;
 `
+export const Title = styled.p`
+/* margin-bottom: 24px; */
+/* margin-top: -160px; */
+font-size: 19px;
+line-height: 1.2;
+text-align: center;
+`
+
+export const Price = styled.div`
+/* margin-bottom: 24px; */
+margin-top: -130px;
+font-size: 24px;
+line-height: 1.2;
+`
+
 export const Image = styled.div`
 height: 100%;
 width: 100%;
 object-fit: fill;
+`
+export const Button = styled.button`
+border-radius: 4px;
+background: ${({ primary }) => (primary ? '#333947' : '#333947')};
+white-space: nowrap;
+padding: ${({ big }) => (big ? '12px 64px' : '10px 20px')};
+color: #fff;
+font-size: ${({ fontBig }) => (fontBig ? '20px' : '16px')};
+outline: none;
+border: none;
+cursor: pointer;
+
+&:hover {
+    transition: all 0.3s ease-out;
+    background: #fff;
+    background: ${({ primary }) => (primary ? '#333947' : '#333947')};
+}
+`
+
+export const Rating = styled(AiOutlineStar)`
+margin-right: 0.5rem;
+margin-top: -120px;
+font-size: 20px;
 `
--- src/components/StoreSite/StoreSite.js
+++ src/components/StoreSite/StoreSite.js
@@ -1,7 +1,8 @@
 import React from 'react'
-import { StoreContainer, StoreGrids, ImgStore, ItemHeadline, Image } from './StoreSite.elements';
+import { StoreContainer, StoreGrids, ImgStore, ItemHeadline, Image, Button, Title, Price } from './StoreSite.elements';
 
-const StoreSite = ({ imgStoreOne, imgStoreTwo, imgStoreThree, imgStoreFour, imgStoreFive, alt, h1, h2, h3, h4, h5 }) => {
+const StoreSite = ({ imgStoreOne, imgStoreTwo, imgStoreThree, imgStoreFour, imgStoreFive, imgStoreSix, imgStoreSeven, imgStoreEight,
+    alt, h1, h2, h3, h4, h5, h6, h7, h8, price1, price2, price3, price4, price5, price6, price7, price8, t1, t2, t3, t4, t5, t6, t7, t8 }) => {
     return (
         <>
             <StoreContainer>
@@ -9,31 +10,73 @@ const StoreSite = ({ imgStoreOne, imgStoreTwo, imgStoreThree, imgStoreFour, imgS
                     <Image>
                         <ImgStore src={imgStoreOne} alt={alt} />
                     </Image>
+                    <Title>{t1}</Title>
                     <ItemHeadline>{h1}</ItemHeadline>
+                    <Price>{price1}</Price>
+                    <Button fontBig priary>Add to cart</Button>
                 </StoreGrids>
                 <StoreGrids>
                     <Image>
                         <ImgStore src={imgStoreTwo} alt={alt} />
                     </Image>
+                    <Title>{t2}</Title>
                     <ItemHeadline>{h2}</ItemHeadline>
+                    <Price>{price2}</Price>
+                    <Button fontBig priary>Add to cart</Button>
                 </StoreGrids>
                 <StoreGrids>
                     <Image>
                         <ImgStore src={imgStoreThree} alt={alt} />
                     </Image>
+                    <Title>{t3}</Title>
                     <ItemHeadline>{h3}</ItemHeadline>
+                    <Price>{price3}</Price>
+                    <Button fontBig priary>Add to cart</Button>
                 </StoreGrids>
                 <StoreGrids>
                     <Image>
                         <ImgStore src={imgStoreFour} alt={alt} />
                     </Image>
+                    <Title>{t4}</Title>
                     <ItemHeadline>{h4}</ItemHeadline>
+                    <Price>{price4}</Price>
+                    <Button fontBig priary>Add to cart</Button>
                 </StoreGrids>
                 <StoreGrids>
                     <Image>
                         <ImgStore src={imgStoreFive} alt={alt} />
                     </Image>
+                    <Title>{t5}</Title>
                     <ItemHeadline>{h5}</ItemHeadline>
+                    <Price>{price5}</Price>
+                    <Button fontBig priary>Add to cart</Button>
+                </StoreGrids>
+                <StoreGrids>
+                    <Image>
+                        <ImgStore src={imgStoreSix} alt={alt} />
+                    </Image>
+                    <Title>{t6}</Title>
+                    <ItemHeadline>{h6}</ItemHeadline>
+                    <Price>{price6}</Price>
+                    <Button fontBig priary>Add to cart</Button>
+                </StoreGrids>
+                <StoreGrids>
+                    <Image>
+                        <ImgStore src={imgStoreSeven} alt={alt} />
+                    </Image>
+                    <Title>{t7}</Title>
+                    <ItemHeadline>{h7}</ItemHeadline>
+                    <Price>{price7}</Price>
+                    <Button fontBig priary>Add to cart</Button>
+                </StoreGrids>
+                <StoreGrids>
+                    <Image>
+                        <ImgStore src={imgStoreEight} alt={alt} />
+                    </Image>
+                    <Title>{t8}</Title>
+                    <ItemHeadline>{h8}</ItemHeadline>
+                    <Price>{price8}</Price>
+                    <Button fontBig priary>Add to cart</Button>
                 </StoreGrids>
             </StoreContainer>
         </>
